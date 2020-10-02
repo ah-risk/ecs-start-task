23d9c214114dea08ecd1dcbe0bd6b3fe9501a76b
Felix Henze <13502755+fhenz@users.noreply.github.com>
2020-10-02 14:31:06 +0200

    Fixed debugging call

--- index.js
+++ index.js
@@ -64,9 +64,8 @@ async function run() {
       const tasks = [startResponse.tasks[0].taskArn];
       try {
         core.debug("Describing tasks: " + tasks);
-        const descr = await ecs.describeTasks({ cluster: startResponse.clusterArn, tasks }).promise();
-        console.log(descr);
-        
+        core.debug(await ecs.describeTasks({ cluster: startResponse.clusterArn, tasks }).promise());
+
         stopResponse = await ecs.waitFor("tasksStopped", { cluster: startResponse.clusterArn, tasks }).promise();
         if (stopResponse.tasks.length <= 0) core.setFailed("No stopped tasks");
         const containers = stopResponse.tasks[0].containers;
